391a1077819d784c8c9b69b73ddef8d9af9b59dd
Vinícius Simões <vinisv2004@gmail.com>
2026-04-14 18:47:42 -0300

    ADD: modal editar carro

--- Código/front-end/js/modalCarro.js
+++ Código/front-end/js/modalCarro.js
@@ -1,5 +1,8 @@
+// Seleção dos elementos
 const modalV = document.getElementById("modalVeiculo");
+const modalEditar = document.getElementById("modalEditarVeiculo");
 
+// Funções para o Modal de CADASTRO
 function abrirModalVeiculo() {
     modalV.style.display = "block";
 }
@@ -8,8 +11,31 @@ function fecharModalVeiculo() {
     modalV.style.display = "none";
 }
 
+// Funções para o Modal de EDIÇÃO
+function abrirModalEditar(veiculoData) {
+    document.getElementById('edit-matricula').value = veiculoData.matricula;
+    document.getElementById('edit-marca').value = veiculoData.marca;
+    document.getElementById('edit-modelo').value = veiculoData.modelo;
+    document.getElementById('edit-lugares').value = veiculoData.lugares;
+    document.getElementById('edit-combustivel').value = veiculoData.combustivel;
+    document.getElementById('edit-status').value = veiculoData.status;
+
+    modalEditar.style.display = "block";
+}
+
+function fecharModalEditar() {
+    modalEditar.style.display = "none";
+}
+
+// Fechar ao clicar fora de qualquer modal
 window.onclick = function(event) {
-    if (event.target == modalV) {
-        fecharModalVeiculo();
+    if (event.target == modalV) fecharModalVeiculo();
+    if (event.target == modalEditar) fecharModalEditar();
+}
+
+function excluirVeiculo() {
+    if (confirm("Tem certeza que deseja excluir este veículo?")) {
+        alert("Veículo excluído com sucesso!");
+        fecharModalEditar();
     }
 }
