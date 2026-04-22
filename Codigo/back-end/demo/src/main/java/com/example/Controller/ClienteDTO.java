302c39381a92e62a00cb442d038f21a11304dc48
Vinícius Simões <vinisv2004@gmail.com>
2026-04-21 23:37:07 -0300

    correção DTO

--- Codigo/back-end/demo/src/main/java/com/example/Controller/ClienteDTO.java
+++ Codigo/back-end/demo/src/main/java/com/example/Controller/ClienteDTO.java
@@ -1,19 +1,19 @@
 package com.example.controller;
 
 import java.time.LocalDate;
-
-import io.micronaut.core.annotation.Introspected;
+import io.micronaut.serde.annotation.Serdeable; 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-@Introspected
+@Serdeable 
 public record ClienteDTO(
     @NotBlank String cpf,
     @NotBlank String nome,
     @NotBlank String rg,
     @NotBlank String emprego,
     @NotBlank String telefone,
-    @NotBlank LocalDate dataNascimento,
+    @NotNull LocalDate dataNascimento, 
     @NotBlank @Email String email, 
     @NotBlank String senha
 ) {}
