1b3d6262c2a212dd672949a67b54681da7eb3576
Luiz Arthur Costa E Costa <166531464+Chapeugenerico@users.noreply.github.com>
2026-04-21 23:56:45 -0300

    Add files via upload

--- Codigo/back-end/demo/src/main/java/com/example/Controller/ClienteDTO.java
+++ Codigo/back-end/demo/src/main/java/com/example/Controller/ClienteDTO.java
@@ -1,26 +1,21 @@
 package com.example.controller;
 
 import java.time.LocalDate;
-import io.micronaut.serde.annotation.Serdeable; 
+
+import io.micronaut.core.annotation.Introspected;
+import io.micronaut.serde.annotation.Serdeable;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
-@Serdeable 
+@Introspected
+@Serdeable
 public record ClienteDTO(
     @NotBlank String cpf,
     @NotBlank String nome,
     @NotBlank String rg,
     @NotBlank String emprego,
     @NotBlank String telefone,
-    @NotNull LocalDate dataNascimento, 
+    @NotBlank LocalDate dataNascimento,
     @NotBlank @Email String email, 
-    @NotBlank String senha,
-    
-    @NotBlank String ruaCliente,
-    @NotBlank String numeroCliente,
-    @NotBlank String bairroCliente,
-    @NotBlank String cidadeCliente,
-    String complementoCliente, 
-    Double rendimento 
+    @NotBlank String senha
 ) {}
